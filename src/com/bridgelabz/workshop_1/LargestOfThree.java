b7df2d26d20ad3236d81e25a05e414297e8f8300
ShreyasNandish <shreyasnandish4@gmail.com>
2022-07-11 11:17:56 +0530

    Added:Largest of three program

--- src/com/bridgelabz/workshop_1/LargestOfThree.java
+++ src/com/bridgelabz/workshop_1/LargestOfThree.java
@@ -1,4 +1,23 @@
 package com.bridgelabz.workshop_1;
 
+import java.util.Scanner;
+
 public class LargestOfThree {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number one :");
+        int n1= sc.nextInt();
+        System.out.println("Enter number Two :");
+        int n2= sc.nextInt();
+        System.out.println("Enter number Three :");
+        int n3= sc.nextInt();
+
+        if(n1>n2 && n1>n3)
+            System.out.println("Number one is largest ");
+        else if(n2>n3)
+            System.out.println("Number Two is largest ");
+        else
+            System.out.println("Number Three is largest ");
+
+    }
 }
